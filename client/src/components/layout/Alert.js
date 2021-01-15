f67849f27d9cc1ab1a33c205f95beb27724e75e7
Emre MUTLU <mutluemre93@gmail.com>
2021-01-15 19:19:26 +0300

    alertAdd

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Alert = (props) => {
+  return <div></div>;
+};
+
+Alert.propTypes = {};
+
+const mapStateToProps = (state) => ({
+  alert,
+}); // Mapping the redux state to a prop in this component in case we have an access to it
+
+export default connect()(Alert);
