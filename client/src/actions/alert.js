68cd7064e4bc03a2eaaf05c0dd6d10e82b95cddc
Emre MUTLU <mutluemre93@gmail.com>
2021-01-15 19:03:01 +0300

    devConnector

---
+++ client/src/actions/alert.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid'; // This package gives us a unique id
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+export const setAlert = (msg, alertType) => (dispatch) => {
+  const id = uuidv4(); // created unique id
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+};
